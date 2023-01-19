3e43c1320b38d9cc3def2a4caa68437916b92ac9
Dinoshna Thathuru <dinoshna@gmail.com>
2023-01-19 23:32:08 +0530

    initialcommit

---
+++ src/test/java/com/deploy/githubcicdactionsdocker/GithubCicdActionsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.githubcicdactionsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
